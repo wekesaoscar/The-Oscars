022a05d5d84fec87b7c3349a188a29b589c9325f
wekesaoscar <147531741+wekesaoscar@users.noreply.github.com>
2023-10-12 00:34:18 +0300

    Add files via upload

---
+++ !HTML/hello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Untitled Document</title>
+    </head>
+    <body>
+        <?php
+            //This is a php comment
+            echo"HELLO WORLD";
+        ?>
+            echo"<p>Say HELLO</p>";
+    </body>
+</html>
